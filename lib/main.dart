85d937158f94c1dc7c18de3afdb1d272783f4735
Stina Terrazas <65819651+stinat@users.noreply.github.com>
2024-05-23 03:36:12 +0000

    RollDiceApp

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:first_app/gradient_container.dart';
+
+void main() {
+  runApp(
+     MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          const Color.fromARGB(255, 33, 5, 109),
+          const Color.fromARGB(255, 68, 21, 149),
+        ),
+      ),
+    ),
+  );
+}
